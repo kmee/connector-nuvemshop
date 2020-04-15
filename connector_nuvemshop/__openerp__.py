7e47ab6ce157c46c6392ef31c80bd12f8f35821a
Luis Felipe Mileo <mileo@kmee.com.br>
2020-04-15 15:14:38 -0300

    [NEW] Connector module base
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ connector_nuvemshop/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Connector-nuvemshop',
+    'summary': """
+        Connector Nuvem Shop""",
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE,Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca',
+    'depends': [
+        'connector_ecommerce',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
