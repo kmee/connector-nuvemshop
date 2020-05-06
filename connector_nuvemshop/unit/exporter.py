6d38e0848e784c11f55b1dc9db585ba762898762
Daniel Sadamo <daniel.sadamo@kmee.com.br>
2020-05-06 12:49:10 -0300

    [FIX] Related action in job

--- connector_nuvemshop/unit/exporter.py
+++ connector_nuvemshop/unit/exporter.py
@@ -172,7 +172,7 @@ def related_action_record(session, job):
     binding_model = job.args[0]
     binding_id = job.args[1]
     record = session.env[binding_model].browse(binding_id)
-    odoo_name = record.odoo_id._name
+    odoo_name = record.openerp_id._name
 
     action = {
         'name': _(odoo_name),
@@ -180,7 +180,7 @@ def related_action_record(session, job):
         'res_model': odoo_name,
         'view_type': 'form',
         'view_mode': 'form',
-        'res_id': record.odoo_id.id,
+        'res_id': record.openerp_id.id,
     }
     return action
 
